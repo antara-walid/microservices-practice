91542a078da28618d66a14a66173a9711493f3a5
antara walid <antara.walid.27@gmail.com>
2022-07-06 08:53:32 +0100

    create Custumer microservice

---
+++ customer/src/main/java/com/Mobiblanc/customer/CustomerRegistrationRequest.java
@@ -0,0 +1,7 @@
+package com.Mobiblanc.customer;
+
+public record CustomerRegistrationRequest(String firstName,
+                                          String lastName,
+                                          String email) {
+
+}
